adcdeb9a82dcca87cbdadda54dbddb494ffc74b4
Ale <Aleu@gmail.com>
2022-06-08 11:49:14 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'; 
+import Footer from './components/Footer.js';
+import React from "react";
 
 class App extends Component {
   render() {
-    return <Navbar></Navbar>
-
+    return <Navbar></Navbar>;
   }
 }
+
+
+
+
+
+
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import { TableFooter } from "@mui/material";
+import React from "react";
+
+function Footer (){
+    return <TableFooter/>
+
+        
+    
+}
+
+
+
+
+
+
+
+
+
+export default Footer;
