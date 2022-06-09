f983b4e10f304f6c4124231edff03b7560bd434a
Maria <maria.carranzavaldes@gmail.com>
2022-06-09 12:27:32 +0200

    #commit para actualizar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,32 @@
 import React from "react";
 import maletaBack from "./imagenes/maletaBack.jpeg";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
 const Home = () => {
   return (
-    <Box id = 'Home-P' style={{}}>
-
-
-        <Box className="item" style={{position:"relative" }}> <img className="maletaBack" src={maletaBack} alt="maletaBack" style={{ }} /> 
-<button className="buttonDescubre" style={{position:"absolute" , top:"70%" , left:"47%" }}>Descubre Mas</button></Box>
-<Box className="item-1"><h1>Experiencia en Barcelona </h1></Box>
-<Box style ={{ display:"inline-flex", gap:"10em"}}><div>ubicacion</div> <div>Transporte</div> <div>Duracion</div></Box>
-
-</Box>
-
-);
-
- }
- export default Home;
+    <Box id="Home-P" style={{}}>
+      <Box className="item" style={{ position: "relative" }}>
+        {" "}
+        <img
+          className="maletaBack"
+          src={maletaBack}
+          alt="maletaBack"
+          style={{}}
+        />
+        <button
+          className="buttonDescubre"
+          style={{ position: "absolute", top: "70%", left: "47%" }}
+        >
+          Descubre Mas
+        </button>
+      </Box>
+      <Box className="item-1">
+        <h1>Experiencia en Barcelona </h1>
+      </Box>
+      <Box style={{ display: "inline-flex", gap: "10em" }}>
+        <div>ubicacion</div> <div>Transporte</div> <div>Duracion</div>
+      </Box>
+    </Box>
+  );
+};
+export default Home;
