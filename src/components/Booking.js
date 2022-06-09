0bae6ecec27423022ee4a66b94c0c68e1ea11c59
Ale <alejapiatti@gmail.com>
2022-06-09 09:01:01 +0200

    Creando Booking y Details

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,3 +1,34 @@
+import React from "react";
+import Logo from './imagenes/Logo.png';
 
+function Booking (){
+    return (
+        <>
+        <img
+            className="logo"
+            style={{ height: "6em" }}
+            src={Logo}
+            alt="Logo"
+          />
+        <p><h2>Formulario de Reserva</h2></p>
+        <a href= "#Modos de pago"><button>Pagar</button>alt="pagar"</a>
+        <p><h3>Start Date</h3></p>
+    </>
+    )}
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+export default Booking;
 
 
---
+++ src/components/Detail.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+import Navbar from "./Navbar.js";
+import 
+function Booking (){
+    return (
+        <>
+        <Navbar/>
+        <img src 
+           
+          />
+        <p><h3>Home-Montaña-Bicicleta</h3></p>  
+        <p><h2>Paseo en bicicleta por el Montseny</h2></p>
+        
+        <a href= "#Booking"><button>Reservar</button>alt="reservar"</a>
+        <Footer/>
+    </>
+    )}
+
+
+
+
+
+
+
+
+
+export default Details;
