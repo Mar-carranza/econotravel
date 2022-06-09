eace0ba7af42d85cbb87940d755c431ec0920a17
hilla-sb <hillsb2302@gmail.com>
2022-06-09 17:00:54 +0200

    cambios-home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 import maletaBack from "./imagenes/maletaBack.jpeg";
+import luzmontaña from "./imagenes/luzmontaña.jpg";
+import playa from "./imagenes/playa.jpg";
+import callebarcelona from "./imagenes/callebarcelona.jpg";
+
 import { Box } from "@mui/material";
 
 const Home = () => {
@@ -23,10 +27,47 @@ const Home = () => {
       <Box className="item-1">
         <h1>Experiencia en Barcelona </h1>
       </Box>
-      <Box style={{ display: "inline-flex", gap: "10em" }}>
-        <div>ubicacion</div> <div>Transporte</div> <div>Duracion</div>
-      </Box>
-    </Box>
+      <Box style={{ display: "inline-flex", gap: "5em" }}>
+        <div>Ubicacion</div> <div>Transporte</div> <div>Duracion</div></Box>
+        
+        <Box className="imagen1" style={{display:"flex",flexDirection: "row" }}>
+    
+        <Box>
+          <img
+          className=""
+          src={luzmontaña}
+          alt=""
+          style={{}} 
+        /><Box ><button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button>
+        <button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button>
+        <button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button></Box></Box>
+
+         <Box><img
+        className=""
+        src={playa}
+        alt="playa"
+        style={{}}
+      /><Box><button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button>
+      <button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button>
+      <button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button></Box></Box>
+
+      <Box><img
+        className=""
+        src={callebarcelona}
+        alt="callebarcelona"
+        style={{}}
+      /><Box><button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button>
+      <button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button>
+      <button className ="Chip One" style ={{borderRadius:"2rem",borderColor:"#B1CC74",color:"#B1CC74"}}>Chip One</button></Box></Box>
+
+</Box>
+
+
+
+
+
+</Box>
+    
   );
 };
 export default Home;
