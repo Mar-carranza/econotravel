fcc580da99f4a50767afa9d6294cb7922e2e7453
dmarcela2022 <104063341+dmarcela2022@users.noreply.github.com>
2022-06-13 16:17:34 +0200

    conflicto resuelto

--- src/components/DetalleReserva_BACKUP_1050.js
+++ src/components/DetalleReserva_BACKUP_1050.js
@@ -38,15 +38,6 @@ function DetalleReserva (){
               <ReadMore>
                   Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny. Una escapada veraniega perfecta para parejas, familias y amigos que nos permitirá conocer nuevos y sorprendentes lugares. El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando asfalto y pistas anchas, culminando en un mirador con magníficas vistas del Vallés y el mar. Desde aquí continuaremos la bajada combinando senderos, pistas y algún tramo de carretera y terreno mixto para visitar la zona de Santa Fe, donde descubriremos la Ermita y el Bosque de las Secuoyas. Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos de una excelente comida casera con butifarra blanca y negra y munxetes del Montseny.
               </ReadMore>
-<<<<<<< HEAD
-            </Box>
-            </Box>
-            </Box> 
-
-            
-
-        </Box>
-=======
               <Box style={{display:'flex', flexDirection:'column', gap:'2rem',width:'50%'}}>
                 <h3 style={{}}>250 €</h3>
               <TextField
@@ -64,7 +55,6 @@ function DetalleReserva (){
           
               
           
->>>>>>> 227393c790532b73bf02673d4c6ffcaab5670563
 
     )
 }
