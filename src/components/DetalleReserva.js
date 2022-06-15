4d553bae894b6b234b74d746df6f8f0c52f15520
Maria <maria.carranzavaldes@gmail.com>
2022-06-15 09:30:15 +0200

    modificacion en detalle reserva

--- src/components/DetalleReserva.js
+++ src/components/DetalleReserva.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { Box } from "@mui/system";
 import Navbar from "./Navbar";
 import bicicleta1 from './imagenes/montseny.jpg';
-import { Typography } from "@mui/material";
-import { TextField } from "@mui/material";
+import { Typography, TextField, Card, FormControl, Button } from "@mui/material";
+import { CardActions } from "@mui/material";
+import CardContent from '@mui/material/CardContent';
+import reactDom from "react-dom";
+
+
 
 
 const ReadMore = ({ children }) => {
@@ -22,7 +26,52 @@ const ReadMore = ({ children }) => {
     );
   };
 
-function DetalleReserva (){
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        250€
+      </Typography>
+      
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        cantidad
+      </Typography>
+     
+    </CardContent>
+    <CardActions>
+      <Button size="small">Reservar</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function DetalleReserva() {
+  return (
+    <Box style={{height: '100vh', padding:'3%'}}>
+            <Navbar />
+          <Box style={{padding:'4%',display:'flex', flexDirection:'row' }}>  
+            <p style={{fontSize:'0.7rem', display:'flex', paddingTop:'4rem' }}> Home  Montaña  Bicicleta </p>
+          </Box>
+          <Box style={{}}> 
+              <Typography variant='h4' component="h2" style={{display:'flex', alignItems:'start'}}>Paseo en bicicleta por el Montseny</Typography>
+          </Box>  
+          <Box style={{display:'flex', justifyContent: 'space-around' }} >
+              <img src={bicicleta1} alt="imagen de bicicleta"/>
+              <ReadMore>
+                  Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny. Una escapada veraniega perfecta para parejas, familias y amigos que nos permitirá conocer nuevos y sorprendentes lugares. El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando asfalto y pistas anchas, culminando en un mirador con magníficas vistas del Vallés y el mar. Desde aquí continuaremos la bajada combinando senderos, pistas y algún tramo de carretera y terreno mixto para visitar la zona de Santa Fe, donde descubriremos la Ermita y el Bosque de las Secuoyas. Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos de una excelente comida casera con butifarra blanca y negra y munxetes del Montseny.
+              </ReadMore>
+          </Box>
+            <Box sx={{ minWidth: 275 }}>
+              <Card variant="outlined">{card}</Card>
+          </Box>
+    </Box>
+  );
+}
+
+          
+
+
+/*
+  function DetalleReserva (){
   const opciones= [1, 2, 3, 4]
     return(
         <Box style={{height: '100vh', padding:'3%'}}>
@@ -38,7 +87,7 @@ function DetalleReserva (){
               <ReadMore>
                   Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny. Una escapada veraniega perfecta para parejas, familias y amigos que nos permitirá conocer nuevos y sorprendentes lugares. El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando asfalto y pistas anchas, culminando en un mirador con magníficas vistas del Vallés y el mar. Desde aquí continuaremos la bajada combinando senderos, pistas y algún tramo de carretera y terreno mixto para visitar la zona de Santa Fe, donde descubriremos la Ermita y el Bosque de las Secuoyas. Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos de una excelente comida casera con butifarra blanca y negra y munxetes del Montseny.
               </ReadMore>
-              <Box style={{display:'flex', flexDirection:'column', gap:'2rem',width:'50%'}}>
+             {/* <Box style={{display:'flex', flexDirection:'column', gap:'2rem',width:'50%'}}>
                 <h3 style={{}}>250 €</h3>
               <TextField
                 id="standard-number"
@@ -50,13 +99,22 @@ function DetalleReserva (){
               />
               <button>Reservar</button>
               </Box>
+              <Box sx={{ minWidth: 275,  }}>
+      <Card variant="outlined"> <h3 style={{}}>250 €</h3>
+              <TextField
+                id="standard-number"
+                
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <button>Reservar</button></Card>
+    </Box>
             </Box>   
-          </Box>    
-          
+          </Box>   
               
           
 
     )
-}
-
-export default DetalleReserva;
+}*/
