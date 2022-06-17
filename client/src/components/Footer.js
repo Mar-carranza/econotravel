11c9755b4702f3effc0d750edb1b0fd01714796a
Ale <alejapiatti@gmail.com>
2022-06-17 13:59:56 +0200

    Reserva

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,22 +6,22 @@ function Footer() {
   return ( 
   
   <TableFooter style ={{width:"100%",display:"block" }}>
-    <Box className="main-footer" style ={{color:"white", backgroundColor:"#D0FFD6",padding:"1em 0 1em 0",display:"flex"}}>
+    <Box className="main-footer" style ={{color:"#710000", backgroundColor:"#B1CC74",padding:"1em 0 1em 0",display:"flex"}}>
          
 
           <Box className ="col">
             <h4 style={{padding:"0.5em 0 0.5em 0"}}>¿Medios de Pago? </h4>
-            <button style={{color:"white",backgroundColor:"#B1CC74", borderColor:"#B1CC74"}}>ver mas</button>
+            <button style={{color:"white",backgroundColor:"#136f63", borderColor:"#B1CC74"}}>ver mas</button>
           </Box>
 
           <Box className="col" >
           <h4 style={{padding:"0.5em 0 0.5em 0"}}>¿Quienes Somos? </h4>
-          <button style={{color:"white",backgroundColor:"#B1CC74", borderColor:"#B1CC74"}}>ver mas</button>
+          <button style={{color:"white",backgroundColor:"#136f63", borderColor:"#B1CC74"}}>ver mas</button>
           </Box>
           
         <Box className ="col">
         <h4 style={{padding:"0.5em 0 0.5em 0"}}>¿Como Funciona? </h4>
-        <button style={{color:"white",backgroundColor:"#B1CC74", borderColor:"#B1CC74"}}>ver mas</button>
+        <button style={{color:"white",backgroundColor:"#136f63", borderColor:"#B1CC74"}}>ver mas</button>
         </Box>
 
         
--- client/src/components/Reserva.js
+++ client/src/components/Reserva.js
@@ -1,23 +1,22 @@
 import React from "react";
 import Logo from '../assets/Logo.png';
 import Box from "@mui/material/Box";
-//import { TextField } from "@material-ui/core/TextField";
-//import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
-//import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
-//import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+import Button from "@mui/material/Button";
 
 
 
 function Reserva (){
-    /*const [value, setValue] = React.useState<Date | null>(null);*/
     
 
-    return (
-    <Box>
-        <Box style={{height:'100vh'}}>
+    return ( 
+    <Box class name= "contenedor" style= {{}}>
+        
+        <Box style={{height:'100vh', justifyContent: "center", display:"flex"}}>
             <img className="logo" style={{ height: "6em" }} src={Logo} alt="Logo"/>    
             <h2>Formulario de Reserva</h2>
         </Box>
+
+        <Box class name= "contenedor" style= {{display:"flex"}}>
         <Box className="form" style={{}}>
          
           <p>Nombre:</p>
@@ -30,19 +29,19 @@ function Reserva (){
             <input className="input"></input>
             <p>Apellidos:</p>
             <input className="input"></input>
+            
         </Box>
-        
-            {/*<LocalizationProvider  dateAdapter={AdapterDateFns}>
-           <DatePicker label="Seleccione una fecha" value={value} 
-            onChange={(newValue) =>{
-                setValue(newValue);
-            }}
-            renderInput={(params) => <TextField {...params} />}/>
-            
-        </LocalizationProvider>*/}
-            
-            
-        
+        <Box>
+            <p style= {{color: "#ff0000"}}>250€</p>
+            <p style= {{fontWeigth: "bold"}}> Experiencia reservada: Paseo en bicicleta por el Montseny. Qty: 1 </p>
+            <Button style= {{backgroundColor:"#B1CC74"}}>Pagar</Button>
+            <p> Garantías de Econotravel:</p>
+            <h6>99% de clientes satisfechos</h6>
+            <h6>Colaboradores de calidad</h6>
+            <h6>Pago 100% seguro</h6>
+
+        </Box>
+        </Box>
         </Box>
          
      )         
